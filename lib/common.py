26eedda6dd70a85012e8cb0d32608b0e1e66c6a4
Xyntax <xyntax@163.com>
2016-06-19 21:33:36 -0400

    add -h/step 3 hydra

--- lib/common.py
+++ lib/common.py
@@ -22,14 +22,24 @@ def openBrowser():
 
 
 def setPaths():
+    # root & output
     paths.ROOT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     paths.OUTPUT_PATH = os.path.join(os.path.join(paths.ROOT_PATH, 'output'), conf.TARGET_DOMAIN)
     if not os.path.exists(paths.OUTPUT_PATH):
         os.mkdir(paths.OUTPUT_PATH)
+
+    # Nmap output
     paths.NMAP_OUTPUT_PATH = os.path.abspath(os.path.join(paths.OUTPUT_PATH, 'nmap.txt'))
+
+    # subDomains output
     paths.DOMAIN_DICT = os.path.join(paths.ROOT_PATH, 'subDomainsBrute/dict')
     paths.DOMAIN_OUTPUT_PATH = os.path.abspath(os.path.join(paths.OUTPUT_PATH, 'subDomain.txt'))
     paths.IP_PATH = os.path.abspath(os.path.join(paths.OUTPUT_PATH, 'ips.txt'))
+
+    # dicts
+    paths.DICT_PATH = os.path.abspath(os.path.join(paths.ROOT_PATH, 'dics'))
+    paths.USR_LIST = os.path.abspath(os.path.join(paths.DICT_PATH, 'usr10.txt'))
+    paths.PWD_LIST = os.path.abspath(os.path.join(paths.DICT_PATH, 'pwd40.txt'))
 
 
 def initOptions():
@@ -48,15 +58,13 @@ def getIPs():
 
 
 def runNmap(auto=False):
-    if sys.argv[2]:
-        auto = True
     while True:
         try:
             base_command = 'nmap -iL ' + paths.IP_PATH
 
             print '\n' + '[e.g.] ' + base_command + ' -p1-65535 -sV --open --script=auth, brute, vuln'
-            print '[1] --script=auth'
-            print '[2] -p1-65535 -sV --script=auth'
+            print '[1] --script=auth,brute'
+            print '[2] -p1-65535 -sV --script=auth,default,brute'
 
             input_command = raw_input(' > ' + base_command) if not auto else 1
             if str(input_command) is '1':
@@ -65,6 +73,35 @@ def runNmap(auto=False):
                 input_command = '-p1-65535 -sV --script=auth,default,brute'
             else:
                 pass
+            command = base_command + ' ' + input_command
+
+            logger.log(CUSTOM_LOGGING.SUCCESS, 'Execute Command: ' + command)
+            p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            c = p.stdout.read()
+            print c
+            file = open(paths.NMAP_OUTPUT_PATH, 'w')
+            file.write(c)
+            file.close()
+            # retval = p.wait()
+
+            if auto:
+                break
+        except KeyboardInterrupt:
+            break
+            # ip = conf.TARGET_DOMAIN
+
+
+def runHydra(auto=False):
+    '''
+    hydra -L userlist.txt -P pws.txt -M targets.txt ssh
+    '''
+    while True:
+        try:
+            base_command = 'hydra -M ' + paths.IP_PATH + ' -L ' + paths.USR_LIST + ' -P ' + paths.PWD_LIST
+
+            print '\n' + '[e.g.] ' + base_command + ' ssh'
+
+            input_command = raw_input(' > ' + base_command) if not auto else 'ssh'
             command = base_command + ' ' + input_command
 
             logger.log(CUSTOM_LOGGING.SUCCESS, 'Execute Command: ' + command)
--- subnmap.py
+++ subnmap.py
@@ -1,18 +1,33 @@
 # !/usr/bin/env python
 #  -*- coding: utf-8 -*-
+import sys
 from lib.data import paths, conf, logger
 from lib.log import CUSTOM_LOGGING
-from lib.common import initOptions, runNmap, getIPs
+from lib.common import initOptions, runNmap, getIPs, runHydra
 from subDomainsBrute.subDomainsBrute import bruteInterface
+
+if '-h' in sys.argv:
+    print 'Usage:\n python subnmap.py DOMAIN [auto] [j1] [j2] [j3]\n' \
+          'Example:\n python subnmap.py baidu.com' \
+          '\n python subnmap.py wooyun.org auto j1' \
+          '\n\nArgument:\n DOMAIN\t base domain for scanning.' \
+          '\nOptions:\n j1\tjump subDomainsBrute step.' \
+          '\n j2\tjump Nmap step.' \
+          '\n j3\tjump Hydra step.' \
+          '\n auto\trun all steps automaticly with default settings.'
+    sys.exit(0)
+
+auto = True if 'auto' in sys.argv else False
 
 initOptions()
 # logger.log(CUSTOM_LOGGING.SYSINFO, paths)
 logger.log(CUSTOM_LOGGING.SUCCESS, 'Start subDomainsBrute => ' + conf.TARGET_DOMAIN)
-bruteInterface()
+if 'j1' not in sys.argv: bruteInterface()
 
 logger.log(CUSTOM_LOGGING.SUCCESS, 'Extracting IPs from subDomains...')
 getIPs()
 
 logger.log(CUSTOM_LOGGING.SUCCESS, 'Staring Nmap... [ctrl-C] to next step.')
-runNmap()
+if 'j2' not in sys.argv: runNmap(auto)
 
+if 'j3' not in sys.argv: runHydra(auto)
